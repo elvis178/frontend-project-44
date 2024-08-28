5fc98555f3a4cfa9bbd7395d87e19ab091b7003f
elvis178 <elvisss178@gmail.com>
2024-08-29 00:16:45 +0300

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const greetUser = () => {
-    const name = readlineSync.question('May I have your name?');
-    console.log(`Hello, ${name}!`);
+const greetUser = () => {
+  const name = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${name}!`);
 };
-
+export default greetUser;
