82617e35599a509fba75702dd2d79f1669604c19
elvis178 <elvisss178@gmail.com>
2024-08-31 10:30:03 +0300

    Fix eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -8,11 +8,10 @@ import pluginJs from '@eslint/js';
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-///const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
-const compat = new FlatCompat({
+const compat = new FlatCompat({ 
   baseDirectory: __dirname,
-  recommendedConfig: pluginJs.configs.recommended
-});
+  recommendedConfig: pluginJs.configs.recommended,
+ });
 
 export default [
   {languageOptions: { globals: globals.node }},
