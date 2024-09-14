950025b231678bbb4ecd2f215ac718e9a92f45f0
elvis178 <elvisss178@gmail.com>
2024-09-15 00:07:04 +0300

    Update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,23 +3,28 @@ import getRandomNumber from '../utils.js';
 
 const description = 'What number is missing in the progression?';
 
+const generateProgression = (start, step, length) => {
+  const progression = [];
+  for (let i = 0; i < length; i += 1) {
+    progression.push(start + i * step);
+  }
+  return progression;
+};
+
 const getRound = () => {
   const start = getRandomNumber(1, 100);
   const step = getRandomNumber(1, 10);
   const length = getRandomNumber(5, 10);
 
-  const progression = [];
-  for (let i = 0; i < length; i += 1) {
-    progression.push(start + i * step);
-  }
+  const progression = generateProgression(start, step, length);
 
   const hiddenIndex = getRandomNumber(0, progression.length - 1);
-  const correctAnswer = String(progression[hiddenIndex]);
+  const answer = String(progression[hiddenIndex]);
 
   progression[hiddenIndex] = '..';
 
   const question = progression.join(' ');
-  return [question, correctAnswer];
+  return [question, answer];
 };
 export default () => {
   playGame(description, getRound);
